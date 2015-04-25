040d9e21388d1a466fe3f73ba6047c1686ada308
Camilo Ordonez <icoj@gmail.com>
2015-04-24 17:46:30 -0700

    added links to buttons

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 $(document).ready(function() { 
 
+// LinkedIn Button
+$('.btn-1').click(function(e){
+      e.preventDefault();
+      window.open('https://www.linkedin.com/in/camiloordonezj');
+   }); 
+
+$('#food-app-btn').click(function(e){
+      e.preventDefault();
+      window.open('https://www.youtube.com/watch?v=S0p8hw1VYGk');
+   }); 
+
+$('#one-app-btn').click(function(e){
+   e.preventDefault();
+   window.open('http://calm-sea-3644.herokuapp.com/');
+});   
+
+$('#game-app-btn').click(function(e){
+   e.preventDefault();
+   window.open('https://floating-gorge-3958.herokuapp.com/');
+});  
+
+
    
-
-   $('.camilo').submit(function(){
-      alert("Its working du");
-   });
-
-
-
-
-
 
 
 
